393b5fcdec2f1f322a29ce6813c82bc877c71d95
AcxesLo <steve.klein.98@web.de>
2026-07-31 12:36:00 +0200

    commit from laptop.

--- src/com/acxes/Main.java
+++ src/com/acxes/Main.java
@@ -3,5 +3,6 @@ package com.acxes;
 public class Main {
     static void main(String[] args) {
         System.out.println("Project CLI");
+        System.out.println("LAPTOP CHANGE ALLA");
     }
 }
